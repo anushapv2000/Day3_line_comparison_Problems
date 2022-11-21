84e16c07076142b12a9ee28a8d0bf0ff748545c7
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-21 21:06:25 +0530

    line comparison

--- src/com/bridgelabz/Linecomparison.java
+++ src/com/bridgelabz/Linecomparison.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 public class Linecomparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter values");
-        int x1=sc.nextInt();
-        int x2=sc.nextInt();
-        int y1=sc.nextInt();
-        int y2=sc.nextInt();
+        System.out.println("Enter values for line 2 ");
+        int x21=sc.nextInt();
+        int x22=sc.nextInt();
+        int y21=sc.nextInt();
+        int y22=sc.nextInt();
+        System.out.println("Enter values for line 1 ");
+        int x11=sc.nextInt();
+        int x12=sc.nextInt();
+        int y11=sc.nextInt();
+        int y12=sc.nextInt();
         System.out.println("Welcome to Line Comparison Computation Program");
-        double len=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-        System.out.println("length of line="+len);
+        double len1=Math.sqrt(Math.pow((x12-x11),2)+Math.pow((y12-y11),2));
+        double len2=Math.sqrt(Math.pow((x22-x21),2)+Math.pow((y22-y21),2));
+        System.out.println("len of line 2 is "+len2 );
+        System.out.println("len of line 1 is "+len1 );
+
+        if(String.valueOf(len2).equals(String.valueOf(len1)))
+        {
+            System.out.println("Two lines are equal");
+        } else if (len2>len1) {
+            System.out.println("Line 2 is greater than Line1");
+        }
+        else {
+            System.out.println("Line 1 is greater than line2");
+        }
+
     }
 }
